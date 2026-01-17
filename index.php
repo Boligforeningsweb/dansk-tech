0556f4100c72171d76dbc3d4266360fbdd76eaf8
Bo Møller <bm@wmh.dk>
2026-01-17 10:26:09 +0100

    Added Jonas to the founders list

--- index.php
+++ index.php
@@ -432,10 +432,10 @@
   <div class="bg-white py-24 sm:py-32 dark:bg-gray-900">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto max-w-3xl text-center">
-        <h2 class="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl dark:text-white">De tre oprindelige bagmænd</h2>
-        <p class="mt-6 text-lg/8 text-gray-600 dark:text-gray-400">Vi er tre glade iværksættere fra Danmark som elsker tech, business og at bygge virksomheder. Vi har fulgt debatten omkring at Danmark måske "falder bagud" internationalt og vi er nok desværre enige. Derfor vil vi gerne prøve at kaste lidt lys på danske tech-firmaer som vi mener er gode alternativer til de store internationale spillere, når man leder efter produkter.</p>
+        <h2 class="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl dark:text-white">De 4 oprindelige bagmænd</h2>
+        <p class="mt-6 text-lg/8 text-gray-600 dark:text-gray-400">Vi er fire glade iværksættere og udviklere fra Danmark som elsker tech, business og at bygge virksomheder. Vi har fulgt debatten omkring at Danmark måske "falder bagud" internationalt og vi er nok desværre enige. Derfor vil vi gerne prøve at kaste lidt lys på danske tech-firmaer som vi mener er gode alternativer til de store internationale spillere, når man leder efter produkter.</p>
       </div>
-      <ul role="list" class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 text-center sm:grid-cols-3 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:justify-items-center">
+      <ul role="list" class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 text-center sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-4 lg:justify-items-center">
         <li>
           <img src="images/bo-moller.png" alt="Bo Møller" width="96" height="96" loading="lazy" class="mx-auto size-24 rounded-full object-cover outline-1 -outline-offset-1 outline-black/5 dark:outline-white/10" />
           <h3 class="mt-6 text-base/7 font-semibold tracking-tight text-gray-900 dark:text-white">Bo Møller</h3>
@@ -450,6 +450,11 @@
           <img src="images/emil-hoejbjerg.png" alt="Emil Højbjerg" width="96" height="96" loading="lazy" class="mx-auto size-24 rounded-full object-cover outline-1 -outline-offset-1 outline-black/5 dark:outline-white/10" />
           <h3 class="mt-6 text-base/7 font-semibold tracking-tight text-gray-900 dark:text-white">Emil Højbjerg</h3>
           <p class="text-sm/6 text-gray-600 dark:text-gray-400">Stifter af bl.a. Boligforeningsweb, EasyPractice, Alunta og PingPuffin</p>
+        </li>
+        <li>
+          <img src="images/jonas.jpg" alt="Jonas Kaas Kristensen" width="96" height="96" loading="lazy" class="mx-auto size-24 rounded-full object-cover outline-1 -outline-offset-1 outline-black/5 dark:outline-white/10" />
+          <h3 class="mt-6 text-base/7 font-semibold tracking-tight text-gray-900 dark:text-white">Jonas Kaas Kristensen</h3>
+          <p class="text-sm/6 text-gray-600 dark:text-gray-400">Freelance-udvikler og administrator på dansktechstack.dk sammen med Bo Møller.</p>
         </li>
       </ul>
     </div>
